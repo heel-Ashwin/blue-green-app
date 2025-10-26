0e8090c1c0e72e52db8c03ebcf8087e5a81678fb
heel-Ashwin <116995118+heel-Ashwin@users.noreply.github.com>
2025-10-26 16:13:32 +0530

    Triggering final pipeline for Green deployment validation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from the Java App! green version");
+        System.out.println("Hello from the Java App! green version now");
     }
 }
